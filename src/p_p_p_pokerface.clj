41dd068d5a40c17c1ede40f35c5dd8c3a2b57c57
Samer Kanjo <samer@kanjo.net>
2018-08-20 18:55:30 -0500

    Finished exercises

--- src/p_p_p_pokerface.clj
+++ src/p_p_p_pokerface.clj
@@ -1,34 +1,73 @@
 (ns p-p-p-pokerface)
 
+(def char->rank {\T 10 \J 11 \Q 12 \K 13 \A 14})
+
 (defn rank [card]
-  nil)
+  (let [[rank _] card]
+    (if (Character/isDigit rank)
+      (Integer/valueOf (str rank))
+      (char->rank rank))))
 
 (defn suit [card]
-  nil)
+  (let [[_ suit] card]
+    (str suit)))
+
+(defn rank-frequency [hand]
+  (vals (frequencies (map rank hand))))
+
+(defn max-rank-frequency [hand]
+  (apply max (rank-frequency hand)))
+
+(defn suit-frequency [hand]
+  (apply max (vals (frequencies (map suit hand)))))
 
 (defn pair? [hand]
-  nil)
+  (= 2 (max-rank-frequency hand)))
 
 (defn three-of-a-kind? [hand]
-  nil)
+  (= 3 (max-rank-frequency hand)))
 
 (defn four-of-a-kind? [hand]
-  nil)
+  (= 4 (max-rank-frequency hand)))
 
 (defn flush? [hand]
-  nil)
+  (= 5 (suit-frequency hand)))
 
 (defn full-house? [hand]
-  nil)
+  (= (sort (rank-frequency hand)) [2 3]))
 
 (defn two-pairs? [hand]
-  nil)
+  (or (= (sort (rank-frequency hand)) [1 2 2])
+      (= (sort (rank-frequency hand)) [1 4])))
 
 (defn straight? [hand]
-  nil)
+  (let [ranks (map rank hand)
+        ace? (= 14 (apply max ranks))
+        two? (= 2 (apply min ranks))
+        low-ace-hand? (and ace? two?)
+        seq-ranks? (fn [r]
+                     (let [sorted-ranks (sort r)
+                           lowest-rank (first sorted-ranks)]
+                       (= sorted-ranks (range lowest-rank (+ lowest-rank 5)))))]
+    (if low-ace-hand?
+      (seq-ranks? (replace {14 1} ranks))
+      (seq-ranks? ranks))))
 
 (defn straight-flush? [hand]
-  nil)
+  (and (flush? hand) (straight? hand)))
+
+(defn high-card? [hand]
+  true)
 
 (defn value [hand]
-  nil)
+  (let [matching-hand? (fn [checker] ((first checker) hand))
+        checkers #{[high-card? 0]
+                   [pair? 1]
+                   [two-pairs? 2]
+                   [three-of-a-kind? 3]
+                   [straight? 4]
+                   [flush? 5]
+                   [full-house? 6]
+                   [four-of-a-kind? 7]
+                   [straight-flush? 8]}]
+    (apply max (map second (filter matching-hand? checkers)))))
